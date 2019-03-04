d8f0253763e02d33734b8fba21db30f6b12d891c
Philip M <philipwaltermay@gmail.com>
2019-03-04 17:27:18 -0600

    Putting all code together with event listener function

--- static/js/app.js
+++ static/js/app.js
@@ -1,43 +1,32 @@
 // from data.js
 var tableData = data;
 
+// Reference button
+var submit = d3.select("#filter-btn");  
 
-// Creates datetime mapping
-var datetimes = tableData.map(dt => dt.datetime);
-
-// Creates city mapping
-var cities = tableData.map(c => c.city);
-
-// Creates state mapping
-var states = tableData.map(s => s.state);
-
-// Creates country mapping
-var countries = tableData.map(co => co.country);
-
-// Creates shape mapping
-var shapes = tableData.map(sh => sh.shape);
-
-// Creates comment mapping
-var comments = tableData.map(com => com.comment);
-
-// Designs function for filtering datetime
-function dateFilter(x) {
-    return x.datetime === '1/1/2010';
-}
-
-// Test function and filter
-var dateInForm = tableData.filter(dateFilter);
-console.log(dateInForm);
-
-// Reference table body
-var tbody = d3.select("tbody");
-
-// Loop through selected data, append rows, add table data
-dateInForm.forEach((formDate) => {
-    var row = tbody.append("tr");
-    Object.entries(formDate).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
-  });
-  
+// Create function when event occurs 
+submit.on('click',function () {
+    // Prevent page from refreshing
+    d3.event.preventDefault();
+    // Reference input
+    var inputField = d3.select(".form-control");
+    // Pull input value
+    var inputValue = inputField.property("value");
+    console.log(inputValue);
+    // Designs function for filtering datetime
+    function dateFilter(x) {
+        return x.datetime === inputValue;}    
+    // Filter data
+    var filteredData = tableData.filter(dateFilter);
+    console.log(filteredData);
+    // Reference for table data
+    var tbody = d3.select("tbody");
+    // Append data to table
+    filteredData.forEach((y) => {
+        var row = tbody.append("tr");
+            Object.entries(y).forEach(([key, value]) => {
+              var cell = tbody.append("td");
+              cell.text(value);
+            });
+        });
+});
