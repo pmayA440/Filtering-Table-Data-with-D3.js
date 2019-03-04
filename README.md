ce764e0e51752d8bdc607c452176669a7cf66cb8
Philip M <philipwaltermay@gmail.com>
2019-03-04 10:53:05 -0600

    Add mapping to each object in tableData

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Filtering-Table-Data-with-D3.js
-Develop web code to create a dynamic filter for large table data.  Project utilizes: JavaScript, HTML, CSS, and D3.js
+Develop web code to create a dynamic filter for large table data.  Project utilizes: JavaScript, HTML, CSS, and D3.js.
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,42 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// 
+
+
+// Creates datetime mapping
+var datetimes = tableData.map(function(dt) {
+    return dt.datetime;
+    console.log(datetimes);
+});
+
+// Creates city mapping
+var cities = tableData.map(function(c) {
+    return c.city;
+    console.log(cities);
+});
+
+// Creates state mapping
+var states = tableData.map(function(s) {
+    return s.state;
+    console.log(states);
+});
+
+// Creates country mapping
+var countries = tableData.map(function(co) {
+    return co.country;
+    console.log(countries);
+});
+
+// Creates shape mapping
+var shapes = tableData.map(function(sh) {
+    return sh.shape;
+    console.log(shapes);
+});
+
+// Creates comment mapping
+var comments = tableData.map(function(com) {
+    return com.comment;
+    console.log(comments);
+});
+
